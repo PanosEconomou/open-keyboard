370b271c10d5c5914dbc291ed70832eb93263af1
Panos Oikonomou <panos.economou.v@gmail.com>
2026-07-20 17:09:36 +0300

    tighter tolerances

--- keycaps/generator.py
+++ keycaps/generator.py
@@ -2,8 +2,8 @@ from OPK.opk import *
 from cadquery.vis import show
 from cadquery import exporters
 
-capu    = keycap(unitX = 1, angle = 0, height = 10, thickness = 1.6)
-capuu   = keycap(unitX = 2, angle = 0, height = 10, thickness = 1.6, convex = True)
+capu    = keycap(unitX = 1, angle = 0, height = 7.5, thickness = 1.5)
+capuu   = keycap(unitX = 2, angle = 0, height = 7.5, thickness = 1.5, convex = True)
 
 exporters.export(capu,  'keycap_1u.step')
 exporters.export(capuu, 'keycap_2u.step')
